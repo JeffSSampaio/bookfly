c3d44d7f418539e3974c513631521564d0dba8c9
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-11 19:26:46 -0300

    feat:Adicionado bookfly-api (springboot + java)  + Interface(html+css+js)

---
+++ bookfly-api/src/main/java/com/jefferson/bookfly_api/BookflyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jefferson.bookfly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflyApiApplication.class, args);
+	}
+
+}
