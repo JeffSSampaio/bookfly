a5ad807aa0a44629d15d8f42c88c9824d71a87de
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-12 00:45:59 -0300

    feat: adicionando  heder, input de pesquisa e navbar com movimento para fechar e abrir ,além de butões dentro do menu

---
+++ Interface/scripts/h-navbar.js
@@ -0,0 +1,38 @@
+
+
+function btnMenu(){
+const menu = document.getElementById("icon-menu")
+const navbar= document.getElementById("h-navbar-right")
+const closeBtn = document.getElementById("close-menu");
+   menu.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+
+} 
+
+buttons = [ 
+    "<a href='/interface/pages/perfil.html'>Perfil</a>",
+    "<a href='/interface/pages/meus-livros.html'>Meus Livros</a>",
+    "<a href='/interface/pages/login.html'>Desconectar</a>",
+
+]
+
+content = document.getElementById("menu-content")
+
+buttons.forEach(element => {
+    
+    content.innerHTML+=
+    ` 
+    <div class='btns-menu'>
+    ${element}
+    </div>
+    `
+
+});
