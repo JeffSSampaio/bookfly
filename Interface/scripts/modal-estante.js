0add6247a0d6ff9af0b605c6e173de0b11aa5d74
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-14 16:54:00 -0500

    feat: criado corpo inicial do modal de estante

---
+++ Interface/scripts/modal-estante.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+
+
+
+
+
+function openModal(){
+
+    const modal = document.querySelector('.modal-bookcase');
+     modal.style.display='block';
+}
+
+function closeModal(){
+      const modal = document.querySelector('.modal-bookcase');
+     modal.style.display='none';
+}
