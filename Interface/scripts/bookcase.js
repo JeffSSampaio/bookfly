befed44eff34d5806a385c6dea6b54b4e01119eb
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-14 16:03:08 -0500

    feat: criado tela meus livros

---
+++ Interface/scripts/bookcase.js
@@ -0,0 +1,95 @@
+
+
+
+var bookcases = [
+    {
+        'name':"Terror em Dobro",
+        'books': [
+        {
+        'cover':'../assets/livro.png',
+        'title':'girassol na Janela',
+        'author':'Ganymédes José',
+         },
+          {
+        'cover':'../assets/livro.png',
+        'title':'girassol na Janela',
+        'author':'Ganymédes José',
+         },
+         
+         
+        ]
+    },
+      {
+        'name':"Terror em Dobro2",
+        'books': [
+        {
+        'cover':'../assets/livro.png',
+        'title':'girassol na Janela',
+        'author':'Ganymédes José',
+         },
+          {
+        'cover':'../assets/livro.png',
+        'title':'girassol na Janela',
+        'author':'Ganymédes José',
+         },
+        ]
+    },
+    
+]
+
+
+var bookcase = document.getElementById('bookcase');
+ const b = document.getElementById('c-books')
+
+bookcases.forEach(element => {
+   
+
+    bookcase.innerHTML += `
+    
+    
+        <div class="title-bookase-container">
+            <h1>${element.name}</h1>
+            <span>  <img src="/Interface/assets/iconAdd.svg" alt=""></span>
+            <span>  <img src="/Interface/assets/iconEdit.svg" alt=""></span>
+        </div>
+       
+
+ ` 
+    
+  
+    let livros = ""
+
+
+    element.books.forEach(book => {
+        
+        livros +=  ` 
+  
+
+            <div class="book">
+                  <img src=${book.cover} alt="">
+                 <div class="book-info">
+                    <p>${book.title.toUpperCase()}</p>
+                     <p>${book.author}</p>
+                </div>
+            </div>
+
+    
+    `
+ 
+    })
+
+    bookcase.innerHTML +=
+    ` 
+    <div class='books-container'>
+
+    ${livros}
+
+    </div>
+    `
+
+   // bookcase.innerHTML += estante(livros)
+
+   
+});
+
+
