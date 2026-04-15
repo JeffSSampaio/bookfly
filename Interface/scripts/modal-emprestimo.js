622e1f80af6b5ca5ef28f62ac5532ec88bde951a
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-15 16:44:21 -0500

    feat:adicionado modal-emprestimo

---
+++ Interface/scripts/modal-emprestimo.js
@@ -0,0 +1,12 @@
+
+
+function openModal(){
+
+    const modal = document.querySelector('.modal-emprestimo');
+     modal.style.display='block';
+}
+
+function closeModal(){
+      const modal = document.querySelector('.modal-emprestimo');
+     modal.style.display='none';
+}
