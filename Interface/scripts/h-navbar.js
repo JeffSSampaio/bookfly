c92dfbf6771975be89b094000329e8663202ec39
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-16 16:48:02 -0500

    fix: separando botões de usuario e admin

--- Interface/scripts/h-navbar.js
+++ Interface/scripts/h-navbar.js
@@ -16,12 +16,17 @@ closeBtn.addEventListener("click", () => {
 
 
 } 
-var buttons = [ 
+var buttonsProfile = [ 
     "<a href='../pages/perfil.html'>Perfil</a>",
-    "<a href='../pages/homepage.html'>HomePage</a>",
+    "<a href='../pages/a-homepage.html'>HomePage</a>",
     "<a href='../pages/meus-livros.html'>Minhas Estantes</a>",
     "<a href='../pages/login.html'>Desconectar</a>",
 
+]
+
+var buttonAdmin =[
+    "<a href='../pages/a-homepage.html'>Homepage</a>",
+    "<a href='../pages/login.html'>Desconectar</a>",
 ]
 
 
@@ -29,16 +34,35 @@ var buttons = [
  var page = new URL(window.location.href)
 
  if (page.pathname == '/Interface/pages/homepage.html') {
-    var index = buttons.indexOf("<a href='../pages/homepage.html'>HomePage</a>")
-    buttons.splice(index,1)
+    var index = buttonsProfile.indexOf("<a href='../pages/homepage.html'>HomePage</a>")
+    buttonsProfile.splice(index,1)
  };
 
+if (page.pathname.substr(-16).includes('/a-homepage.html')) {
+    var index = buttonAdmin.indexOf("<a href='../pages/a-homepage.html'>HomePage</a>")
+    buttonAdmin.splice(index,0)
+ };
 
 
 content = document.getElementById("menu-content")
 
-buttons.forEach(element => {
-    
+
+if(page.pathname.substr(-16).startsWith("/a-")){
+
+        buttonAdmin.forEach(element=>{
+
+            content.innerHTML+=
+             ` 
+            <div class='btns-menu'>
+                ${element}
+                    </div>
+    `
+        })
+
+    } else{
+    buttonsProfile.forEach(element => {
+   
+   
     content.innerHTML+=
     ` 
     <div class='btns-menu'>
@@ -47,3 +71,11 @@ buttons.forEach(element => {
     `
 
 });
+
+    }
+
+
+
+
+
+ 
