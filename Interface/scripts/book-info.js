d55f2a9b7e40a788b0f1ce8ada72013ff714fc70
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-17 16:55:24 -0500

    fix:alterando pagina de livro

--- Interface/scripts/book-info.js
+++ Interface/scripts/book-info.js
@@ -23,15 +23,15 @@ container_book_info.innerHTML+=`
       <div class="b-book-info">
 
             <h1 class="t-book">${book.title.toUpperCase()}</h1>
-            <h2 class="t-author">${book.author.toUpperCase()}</h2>
+            <h2 class="t-author">${book.author}</h2>
 
-           <div class="s-book">
+           <div class="s-book-status">
                 <h2>Status</h2>
                     <p class="s-field-status">${book.status.toUpperCase()}</p>
            </div> 
             <div class="s-book">
                 <h2>Sumario</h2>
-                <p class="s-field-status ">${book.summary.toUpperCase()}</p>
+                <p class="s-field-status ">${book.summary}</p>
            </div> 
 
 
