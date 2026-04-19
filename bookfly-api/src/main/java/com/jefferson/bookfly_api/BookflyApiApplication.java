7078f67c703c8480505ca73974a5d6e3760079ac
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-04-19 19:31:06 -0300

    feat: inicializando ambiente + adicionando biblioteca spring security + cirando variaveis de ambiente para postgree

--- bookfly-api/src/main/java/com/jefferson/bookfly_api/BookflyApiApplication.java
+++ bookfly-api/src/main/java/com/jefferson/bookfly_api/BookflyApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class BookflyApiApplication {
 
 	public static void main(String[] args) {
