318ec3739e8df46505efbdd1bc8b32425fa5cf86
JeffSSampaio <jeffersonsamp2012@gmail.com>
2026-06-20 00:15:44 -0300

    feat:adicionando componente de btn

---
+++ bookfly-interface/src/components/ComponentButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-btn 
+      :color="color" 
+      :size="size" 
+      :variant="variant"
+      @click="$emit('click')"
+    >
+        <slot />
+    </v-btn>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+type ThemeColor = 'primary' | 'secondary' | 'success' | 'error' | 'warning' | 'info'
+
+defineProps({
+  color: {
+    type: String as PropType<ThemeColor>,
+    default: 'primary'
+  },
+  size: {
+    type: [String, Number] as PropType<'x-small' | 'small' | 'default' | 'large' | 'x-large' | number>,
+    default: 'default'
+  },
+  variant: {
+    type: String as PropType<'flat' | 'outlined' | 'text' | 'tonal' | 'elevated' | 'plain'>,
+    default: 'elevated'
+  }
+})
+
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.color-blue{}
+
+</style>
